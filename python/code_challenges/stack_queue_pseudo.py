305951853820ad21007bc1eca9088d71d96bc8b5
Monica <ramirez.92.monica@gmail.com>
2023-01-02 17:09:23 -0600

    challenge 10 complete, missing README and whiteboard

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,28 @@
 from data_structures.stack import Stack
+from data_structures.linked_list import Node
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        # self.top = None
+        self.stack = Stack()
+        self.temp = Stack()
+
+    """
+    []
+    """
+    def enqueue(self, value):
+
+        while not self.stack.is_empty():
+            current = self.stack.pop()
+            self.temp.push(current)
+        self.stack.push(value)
+
+        while not self.temp.is_empty():
+            current = self.temp.pop()
+            self.stack.push(current)
+
+    def dequeue(self):
+        return self.stack.pop()
+
+
