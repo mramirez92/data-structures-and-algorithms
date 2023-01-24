50ca0ef2d21cd247467c9e80b846b6724c5b39c4
Monica Ramirez <109626849+mramirez92@users.noreply.github.com>
2023-01-23 23:18:06 -0600

    Update sorting.py

--- python/data_structures/sorting.py
+++ python/data_structures/sorting.py
@@ -14,3 +14,24 @@ def insertion_sort(array):
         array[j + 1] = key_item
     print(array)
     return array
+   
+def test_insertion_sort():
+    # Test case 1: Check if the function sorts an array correctly
+    array = [3, 2, 5, 1, 4]
+    result = insertion_sort(array)
+    expected = [1, 2, 3, 4, 5]
+    assert result == expected
+    print('pass')
+
+def test_insertion_sort_2():
+    array = [-2,3,5,7,13,11]
+    result = insertion_sort(array)
+    expected = [-2,3,5,7,11,13]
+    assert result == expected
+    print('pass')
+
+
+test_insertion_sort()
+
+test_insertion_sort_2()
+  
