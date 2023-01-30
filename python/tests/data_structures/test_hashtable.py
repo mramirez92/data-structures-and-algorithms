8ffc610d9a6fb290565c69d7cb1d26d96a92a80f
Monica <ramirez.92.monica@gmail.com>
2023-01-29 22:55:32 -0600

    add function testing

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,7 +6,6 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
 def test_get_apple():
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
@@ -15,8 +14,10 @@ def test_get_apple():
     assert actual == expected
 
 
+# ask about display?# @pytest.mark.skip("TODO")
 @pytest.mark.skip("TODO")
-def test_internals():
+def test_internals():  # @pytest.mark.skip
+
     hashtable = Hashtable(1024)
     hashtable.set("ahmad", 30)
     hashtable.set("silent", True)
@@ -32,3 +33,48 @@ def test_internals():
     expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
 
     assert actual == expected
+
+
+# @pytest.mark.skip
+def test_set():
+    ht = Hashtable(10)
+    ht.set("bones", 206)
+    ht.set("ligaments", 900)
+    assert ht._buckets[ht._hash("bones")].head.value == ["bones", 206]
+    assert ht._buckets[ht._hash("ligaments")].head.value == ["ligaments", 900]
+
+
+# @pytest.mark.skip
+def test_get():
+    ht = Hashtable()
+    ht.set("whiskers", "cat")
+    actual = ht.get("whiskers")
+    expected = "cat"
+    assert actual == expected
+
+
+# @pytest.mark.skip
+def test_key_return():
+    ht = Hashtable()
+    ht.set("Rush Hour", "Comedy")
+    ht.set("Halloween", "Horror")
+    ht.set("Terminator 2", "Action")
+    assert ht.keys() == ["Rush Hour", "Halloween", "Terminator 2"]
+    assert ht.keys() != ["Comedy", "Horror", "Action"]
+
+# @pytest.mark.skip
+def test_has():
+    ht = Hashtable()
+    ht.set("Shark", "fish")
+    ht.set("Platypus", "mammal")
+    ht.set("Boa", "reptile")
+    assert ht.has("Boa") == True
+    assert ht.has("Flamingo") == False
+
+
+# @pytest.mark.skip
+def test_hash():
+    ht = Hashtable()
+    assert ht._hash("Python") == 934
+    assert ht._hash("Pythons") == 47
+    assert ht._hash("python") != 934
